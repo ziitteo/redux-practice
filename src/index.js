39149d735fa4ad853475817c5ea91e2d07911757
Ziitteo <ziitteo@gamil.com>
2024-08-24 22:21:14 +0900

    코드 수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import App from './App';
 
--- src/redux/reducer/reducer.js
+++ src/redux/reducer/reducer.js
@@ -2,10 +2,11 @@ const initialState = {
   fillPercentage: 0,
 };
 
-function reducer(state = initialState, action) {
+function reducer(action, state = initialState) {
   if (action.type === 'INCREMENT') {
     return { ...state, fillPercentage: Math.min(state.fillPercentage + 20, 100) }; // 최대 100%까지 채우기
-  } else if (action.type === 'DECREMENT') {
+  }
+  if (action.type === 'DECREMENT') {
     return { ...state, fillPercentage: Math.max(state.fillPercentage - 20, 0) }; // 최소 0%까지 비우기
   }
   return { ...state };
